a070fec5b303a8e4e40dc526686b41eb21f9c9e5
Duncan Drennan <duncan@engineersimplicity.com>
2021-06-07 02:09:52 +0200

    Output test results to JSON file
    * Also output results/limits to command line output

--- include/CppUTest/ProdUTestOutput.h
+++ include/CppUTest/ProdUTestOutput.h
@@ -2,6 +2,7 @@
 #define D_ProdUTestOutput_h
 
 #include "TestOutput.h"
+#include "PlatformSpecificFunctions.h"
 
 class ProdUTestOutput: public ConsoleTestOutput
 {
@@ -9,12 +10,24 @@ public:
     ProdUTestOutput(void);
     virtual ~ProdUTestOutput(void);
 
+    virtual void printTestsStarted()_override;
+    virtual void printTestsEnded(const TestResult& result)_override;
+    virtual void printCurrentTestStarted(const UtestShell& test)_override;
     virtual void printCurrentTestEnded(const TestResult& res) _override;
+
     virtual void printFailure(const TestFailure& failure) _override;
 
 protected:
+    PlatformSpecificFile file_;
+    int errorCode_;
+    bool finalTest_;
+
     virtual void printEclipseErrorInFileOnLine(SimpleString file, int lineNumber);
     virtual void printFileAndLineForFailure(const TestFailure& failure) _override;
+
+    virtual void openFileForWrite(const SimpleString& fileName);
+    virtual void writeToFile(const SimpleString& buffer);
+    virtual void closeFile();
 };
 
 #endif
--- src/CppUTest/ProdUTestOutput.cpp
+++ src/CppUTest/ProdUTestOutput.cpp
@@ -1,7 +1,10 @@
 #include "CppUTest/TestHarness.h"
 #include "CppUTest/ProdUTestOutput.h"
 
-ProdUTestOutput::ProdUTestOutput()
+#include <time.h>
+
+ProdUTestOutput::ProdUTestOutput() :
+     file_(NULL), errorCode_(0), finalTest_(false)
 {
     verbose();
 }
@@ -10,23 +13,93 @@ ProdUTestOutput::~ProdUTestOutput()
 {
 }
 
+void ProdUTestOutput::printTestsStarted()
+{
+    // TODO: open a directory
+    // TODO: specify log filename or suffix from command line
+
+    time_t tm = time(NULL);
+    char dateTime[80];
+    struct tm *tmp = gmtime(&tm);
+    strftime(dateTime, 80, "%Y%m%d_%H%M%S", tmp);
+    
+    SimpleString fileName = StringFromFormat("%s_log.json", dateTime);
+
+    print("Logging to:");
+    print(fileName.asCharString());
+    print("\n");
+
+    openFileForWrite(fileName);
+    writeToFile("{");
+    writeToFile(StringFromFormat("\"ts\":\"%lu\",", (unsigned long)tm));
+    strftime(dateTime, 80, "%Y-%m-%dT%H:%M:%SZ", tmp);
+    writeToFile(StringFromFormat("\"date\":\"%s\",", dateTime));
+    writeToFile("\"tests\":[");
+}
+
+void ProdUTestOutput::printTestsEnded(const TestResult& result)
+{
+    TestOutput::printTestsEnded(result);
+
+    writeToFile("],");
+    writeToFile(StringFromFormat("\"error\":%d,", errorCode_));
+    writeToFile(StringFromFormat("\"duration\":%lu,", result.getTotalExecutionTime()));
+    writeToFile(StringFrom("\"pass\":") + StringFrom(result.getFailureCount() == 0));
+    writeToFile("}");
+    closeFile();
+}
+
+void ProdUTestOutput::printCurrentTestStarted(const UtestShell& test)
+{
+    print(test.getFormattedName().asCharString());
+
+    writeToFile("{");
+    writeToFile(StringFromFormat("\"group\":\"%s\",", test.getGroup().asCharString()));
+    writeToFile(StringFromFormat("\"name\":\"%s\",", test.getName().asCharString()));
+    writeToFile(StringFrom("\"will_run\":") + StringFrom(test.willRun()) + StringFrom(","));
+
+    if (test.getNext() == NULL)
+    {
+        finalTest_ = true;
+    }
+}
+
 void ProdUTestOutput::printCurrentTestEnded(const TestResult& res)
 {
-    if (res.getCurrentTestPassed())
-    {
-        print(";PASS;");
+    print(";");
+    if (res.getCurrentTestPassed()) {
+        print("PASS;0;");
+    } else {
+        print("FAIL;");
+        print(errorCode_);
+        print(";");
     }
-
+    print(res.getResult());
+    print(";");
+    print(res.getExpected());
+    print(";");
+    print(res.getExpectedMax());
+    print(";");
     print(res.getCurrentTestTotalExecutionTime());
     print(" ms\n");
+
+    writeToFile(StringFromFormat("\"result\":\"%s\",", res.getResult()));
+    writeToFile(StringFromFormat("\"expected\":\"%s\",", res.getExpected()));
+    writeToFile(StringFromFormat("\"expected_max\":\"%s\",", res.getExpectedMax()));
+    writeToFile(StringFromFormat("\"duration\":%lu,", res.getCurrentTestTotalExecutionTime()));
+    writeToFile(StringFrom("\"pass\":") + StringFrom(res.getCurrentTestPassed()));
+    writeToFile("}");
+
+    if (!finalTest_) {
+        writeToFile(",");
+    }
 }
 
 void ProdUTestOutput::printFailure(const TestFailure& failure)
 {
-    print(";FAIL;");
-    print(failure.getErrorCode());
-    print(";");
-    printFileAndLineForFailure(failure);
+    if (errorCode_ == 0) {
+        errorCode_ = failure.getErrorCode();
+    }
 }
 
 void ProdUTestOutput::printFileAndLineForFailure(const TestFailure& failure)
@@ -40,4 +113,19 @@ void ProdUTestOutput::printEclipseErrorInFileOnLine(SimpleString file, int lineN
     print(":");
     print(lineNumber);
     print(";");
+}
+
+void ProdUTestOutput::openFileForWrite(const SimpleString& fileName)
+{
+    file_ = PlatformSpecificFOpen(fileName.asCharString(), "w");
+}
+
+void ProdUTestOutput::writeToFile(const SimpleString& buffer)
+{
+    PlatformSpecificFPuts(buffer.asCharString(), file_);
+}
+
+void ProdUTestOutput::closeFile()
+{
+    PlatformSpecificFClose(file_);
 }
